7cebe2b770969da072f99477c1626a314426e73e
loan anh <loananh0505@gmail.com>
2022-11-02 23:31:37 +0700

    day la commit dau tien

---
+++ main.js
@@ -0,0 +1,16 @@
+
+    alert("chao loan")
+
+
+Đẩy code lên github:
+
+1. git init
+Khởi tạo 1 repo tren may tinh
+2. git remote add origin https://github.com/gnahe2612/firstRepo.git
+Add link cho github cho repo do
+3. git add + Ten File
+add file can day len
+4.git commit -m "day la lan commit dau tien"
+Xac nhan lai cac file day len vaf them comment cho moi commit
+5. git push origin main
+day code len github vao nhanh main
